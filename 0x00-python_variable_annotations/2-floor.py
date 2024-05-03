9c2480161c63773552d65a8cb3edb0ac2555455e
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-04 00:33:28 +0300

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+def to_str(n: float) -> str:
+    """
+    Takes a float n as argument and returns the string representation of the float.
+
+    Args:
+        n (float): A float number.
+
+    Returns:
+        str: The string representation of the float n.
+    """
+    return str(n)
