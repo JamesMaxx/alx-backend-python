62f1673d1a0e908c59a1562bb121145d58e1852f
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-04 00:24:11 +0300

    chore: Add docstring to the add function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """Adds two numbers together.
+
+    This function takes in two numbers (a and b) as inputs and returns
+    their sum.
+
+    Parameters
+    ----------
+    a : float
+        The first number to add.
+    b : float
+        The second number to add.
+ar
+
+    Returns
+    -------
+    float
+        The sum of a and b.
+
+    """
     return a + b
