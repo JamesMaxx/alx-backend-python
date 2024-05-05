c486f28450af7d6ac2f0dc59e918d42634b1a691
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-05 07:21:45 +0300

    Refactor floor function to include docstring and update parameter name

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 '''Rounds a number down to its nearest integer and returns it as an int.
 '''
-
-
 def floor(a: float) -> int:
     '''Computes the floor of a floating-point number.
     '''
