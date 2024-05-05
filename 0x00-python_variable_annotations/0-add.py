ced1880a888cfc15b254aeb60e4fce58aa84a1c9
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-05 06:57:24 +0300

    Refactor add function to include docstring

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
-    '''Adds two floating-point numbers.
-    '''
+    """Adds two numbers together.
+Parameters
+----------
+a : float
+    The first number to add.
+b : float
+    The second number to add.
+
+    Returns
+    -------
+    float
+        The sum of a and b.
+
+    """
     return a + b
