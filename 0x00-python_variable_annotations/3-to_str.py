2768a7d93348b4fdc2a31ac366836d1e6ad17e1f
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-05 07:24:01 +0300

    Refactor to_str function to include docstring and update parameter name

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,18 +1,8 @@
+#!/usr/bin/env python3
+'''Converts a floating-point number to a string.'''
+
 def to_str(n: float) -> str:
-    """Converts a floating-point number to a string.
-
-    This function takes in a floating-point number (n) as input and
-    returns a string representation of that number.
-
-    Parameters
-    ----------
-    n : float
-        The floating-point number to convert to a string.
-
-    Returns
-    -------
-    str
-        A string representation of n.
-
-    """
+    '''
+    Converts a floating-point number to a string.
+    '''
     return str(n)
