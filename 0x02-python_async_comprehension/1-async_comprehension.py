33160e0cf6b7534e643e5b7ca81dba575fd77f5d
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-07 23:30:22 +0300

    1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-import asyncio
+"""Module for async comprehension"""
 from typing import List
 
 async def async_comprehension() -> List[float]:
-    """Async comprehension that returns a list of floats"""
+    """Async comprehension that returns a list of floats.
+
+    Returns:
+        List[float]: A list of floats.
+    """
     return [num async for num in [10.0 for _ in range(10)]]
