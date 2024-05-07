c661d75c0a0e1c895de9f1206e432ee8839cadd3
JamesMaxx <jamesmaxmunene@gmail.com>
2024-05-07 23:13:41 +0300

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """
+    Generates a sequence of random floats.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
